fe29f46360b5c387a83871c19d57cf4ab281db4d
Patrick Koch <patkoch87@gmail.com>
2021-04-30 19:23:18 +0200

    Adaptions for blog 04 files

--- post4/advanced/scripts/register.ps1
+++ post4/advanced/scripts/register.ps1
@@ -1,8 +1,8 @@
 Write-Host "Going to configure Agent..."
 
-Write-Host "C:\agent\config.cmd --unattended --url "https://dev.azure.com/patrickkoch" --auth PAT --pool "TestPool" --token "2j6wyebyfesu2kdezg2t6excp2s2drlc6lgwqauibzelo73o6qma" --agent "myAgent" --replace"
+Write-Host "C:\agent\config.cmd --unattended --url "https://dev.azure.com/patrickkoch" --auth PAT --pool "TestPool" --token "2j6**************************ma" --agent "myAgent" --replace"
 
-C:\agent\config.cmd --unattended --url "https://dev.azure.com/patrickkoch" --auth PAT --pool "TestPool" --token "2j6wyebyfesu2kdezg2t6excp2s2drlc6lgwqauibzelo73o6qma" --agent "myAgent" --replace
+C:\agent\config.cmd --unattended --url "https://dev.azure.com/patrickkoch" --auth PAT --pool "TestPool" --token "2j6**************************ma" --agent "myAgent" --replace
 
 Write-Host "Going to run Agent..."
 
